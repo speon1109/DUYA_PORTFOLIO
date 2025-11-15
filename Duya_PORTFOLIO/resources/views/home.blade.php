c3b798fcf17defb19926def3e699abb4e14a268f
speon <kabutegaming69@gmail.com>
2025-11-15 19:57:30 +0800

    Route and View

---
+++ Duya_PORTFOLIO/resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Portfolio</title>
+</head>
+<body>
+    <section>
+        <h1>Hi! I'm Aaron</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium mollitia neque vel tempore obcaecati ut nostrum sunt. Atque, sed, reiciendis sint porro, facere natus quidem adipisci possimus suscipit consequatur dolores!</p>
+    </section>
+    <hr>
+    <section>
+        <h2>STUFF 1</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium incidunt minus, doloremque corrupti assumenda quam optio animi facilis quos, delectus culpa repellat? Velit, ut quisquam aliquid deleniti dolores nam adipisci!</p>
+        <hr>
+        <h2>STUFF 2</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium incidunt minus, doloremque corrupti assumenda quam optio animi facilis quos, delectus culpa repellat? Velit, ut quisquam aliquid deleniti dolores nam adipisci!</p>
+        <hr>
+        <h2>STUFF 3</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium incidunt minus, doloremque corrupti assumenda quam optio animi facilis quos, delectus culpa repellat? Velit, ut quisquam aliquid deleniti dolores nam adipisci!</p>
+        <hr>
+    </section>
+</body>
+</html>
