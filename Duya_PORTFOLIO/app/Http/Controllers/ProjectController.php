302b6710eba44eb9b1fffebc5fee3d8ffc5ab4e4
speon <kabutegaming69@gmail.com>
2025-11-22 01:17:29 +0800

    Modified ProjectController

--- Duya_PORTFOLIO/app/Http/Controllers/ProjectController.php
+++ Duya_PORTFOLIO/app/Http/Controllers/ProjectController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function addProject(){
+        return view('add');
+    }
+    public function editProject(Project $project){
+        return view('edit', compact('project'));
+    }
+    public function showProjects(){
+        $tags= Tag::all();
+        $projects= auth()->user()->projects()->with('tags')->latest()->get();
+        return view('dashboard', compact('projects','tags'));
+    }
+    public function showProject(Project $project){
+        return view('showProject', compact('project'));
+    }
+    public function destroyProject(Project $project){
+        $project->delete();
+        return;
+    }
 }
