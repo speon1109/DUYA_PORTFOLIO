11d6f9a868b7e39d59fffc48abfb21ead521a5b4
speon <kabutegaming69@gmail.com>
2025-11-22 13:06:21 +0800

    Updated Controller

--- Duya_PORTFOLIO/app/Http/Controllers/ProjectController.php
+++ Duya_PORTFOLIO/app/Http/Controllers/ProjectController.php
@@ -11,14 +11,17 @@ class ProjectController extends Controller
     public function addProject(){ //get
         return view('add');
     }
+
     public function editProject(Project $project){ //get
         return view('edit', compact('project'));
     }
+
     public function showProjects(){ //get
         $tags= Tag::all();
         $projects= auth()->user()->projects()->with('tags')->latest()->get();
         return view('dashboard', compact('projects','tags'));
     }
+
     public function showProject(Project $project){ //get
         return view('showProject', compact('project'));
     }
@@ -50,17 +53,34 @@ class ProjectController extends Controller
         }
         return redirect()->route('showProjects');
     }
+    
+    public function updateProject(Request $request, Project $project){ //patch
+        $fields= $request->validate([
+            'title'=>'string',
+            'description'=>'string',
+            'source_code'=>'string',
+            'tags'=>'array',
+            'tags/*'=>'exists:tags,id',
+            'images'=>'array|image|max:2048',
+            'images.*' => 'image|max:2048',
+        ]);
 
+        $project->update([
+            'title'=>strip_tags($fields['title'] ?? $project->title),
+            'description'=>strip_tags($fields['description'] ?? $project->description),
+            'source_code'=>strip_tags($fields['source_code'] ?? $project->source_code),
+        ]);
 
+        $project->tags()->sync($fields['tags'] ?? $project->tags());
 
-
-
-
-
-
-
-
-
+        if(!empty($request->hasFile('images'))){
+            foreach ($request->file('images') as $image) {
+                $path= $image->store('photos','public');
+                $project->photos()->create(compact('path'));
+            }
+        }
+        return redirect()->route('showProjects');
+    }
 
     public function destroyProject(Project $project){ //delete
         $project->delete();
